9e32ec6c2c3d623d129a9abd7ef24801b36309a9
Adam Drew <addrew@redhat.com>
2025-07-29 08:27:22 -0400

    Sort conversations by last update

--- plugins/convo/src/lib/api.ts
+++ plugins/convo/src/lib/api.ts
@@ -36,8 +36,8 @@ export const getConversations = (
       setConversations(response
           .sort((a: any, b: any) => {
             return (
-              new Date(b.created_at).getTime() -
-              new Date(a.created_at).getTime()
+              new Date(b.updated_at).getTime() -
+              new Date(a.updated_at).getTime()
             );
           })
           .map((conversation: any, idx: number) => {
